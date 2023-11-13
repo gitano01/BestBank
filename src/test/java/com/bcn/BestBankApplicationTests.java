c55391d3e74b66125560e4f392d3968c86fded2c
victor <vramirez.info@gmail.com>
2023-11-13 17:24:24 -0600

    se sube el inicio del proyecto BestBank

---
+++ src/test/java/com/bcn/BestBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
