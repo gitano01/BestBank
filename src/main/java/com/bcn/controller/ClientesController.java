b8b73210381bb09ac3cc9e589678e5417716b0a9
Gabriel Morales <gbmres2704@gmail.com>
2023-11-17 14:54:49 -0600

    Se agrega funcionalidad para la gestión de clientes

--- src/main/java/com/bcn/controller/ClientesController.java
+++ src/main/java/com/bcn/controller/ClientesController.java
@@ -1,5 +1,149 @@
 package com.bcn.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bcn.model.Clientes;
+import com.bcn.service.ClientesDaoServiceImplement;
+
+@RestController
+@RequestMapping(value = "/clientes")
 public class ClientesController {
+	@Autowired
+	ClientesDaoServiceImplement clientesService;
+
+	@GetMapping("/getDatos")
+	public ResponseEntity<?> getDatos() throws Exception {
+		List<List<?>> datos = new ArrayList<List<?>>();
+		try {
+			datos = clientesService.getDatos();
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (datos == null) {
+			return new ResponseEntity<String>("No hay registros", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<List<?>>>(datos, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping("/getClientes")
+	public ResponseEntity<?> getListaClientes() throws Exception {
+		List<Clientes> cliente = null;
+		try {
+			cliente = new ArrayList<Clientes>();
+			cliente = clientesService.getClientes();
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (cliente == null) {
+			return new ResponseEntity<String>("No se hallaron registros", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<Clientes>>(cliente, HttpStatus.OK);
+		}
+	}
+
+	@GetMapping(value = "/getCliente/{clienteId}")
+	public ResponseEntity<?> getCliente(@PathVariable String clienteId) throws Exception {
+		int id = Integer.parseInt(clienteId);
+
+		Clientes cliente = null;
+		try {
+			cliente = clientesService.getCliente(id);
+
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (cliente == null) {
+			return new ResponseEntity<String>("No se hallaron registros", HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<Clientes>(cliente, HttpStatus.OK);
+		}
+	}
+
+	@PostMapping(value = "/addCliente")
+	public ResponseEntity<?> addCliente(@RequestBody Clientes cliente) throws Exception {
+		String response = "";
+		try {
+			response = clientesService.crearCliente(cliente);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (!response.equals("OK")) {
+			return new ResponseEntity<String>(response, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<String>(response, HttpStatus.OK);
+		}
+	}
+
+	@PutMapping(value = "/updateCliente/{clienteId}")
+	public ResponseEntity<?> updateCliente(@RequestBody Clientes cliente, @PathVariable String clienteId)
+			throws Exception {
+		int id = Integer.parseInt(clienteId);
+		String response = "";
+		try {
+			response = clientesService.updateCliente(cliente, id);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (!response.equals("OK")) {
+			return new ResponseEntity<String>(response, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<String>(response, HttpStatus.OK);
+		}
+	}
+
+	@PutMapping(value = "/dropCliente/{clienteId}")
+	public ResponseEntity<?> dropCliente(@RequestBody Clientes cliente, @PathVariable String clienteId)
+			throws Exception {
+		int id = Integer.parseInt(clienteId);
+		String response = "";
+		try {
+			response = clientesService.dropCliente(cliente, id);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (!response.equals("OK")) {
+			return new ResponseEntity<String>(response, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<String>(response, HttpStatus.OK);
+		}
+	}
+
+	@DeleteMapping(value = "/deleteCliente/{clienteId}")
+	public ResponseEntity<?> deleteCliente(@PathVariable String clienteId) throws Exception {
+		int id = Integer.parseInt(clienteId);
+		String response = "";
+		try {
+			response = clientesService.deleteCliente(id);
+		} catch (Exception e) {
+			throw new Exception(e.getMessage());
+		}
+
+		if (!response.equals("OK")) {
+			return new ResponseEntity<String>(response, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<String>(response, HttpStatus.OK);
+		}
+	}
 
 }
--- src/main/java/com/bcn/model/Clientes.java
+++ src/main/java/com/bcn/model/Clientes.java
@@ -1,5 +1,160 @@
 package com.bcn.model;
 
+import java.sql.Timestamp;
+
 public class Clientes {
+	private int clienteId;
+	private String Nombre;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String direccion;
+	private String telefono;
+	private String colonia;
+	private String codigoPostal;
+	private String ciudad;
+	private String estado;
+	private String email;
+	private String estatus;
+	private boolean activo;
+	private Timestamp fechaAlta;
+	private Timestamp fechaBaja;
+	private int sucursalId;
+	private int empleadoId;
+
+	public int getClienteId() {
+		return clienteId;
+	}
+
+	public void setClienteId(int clienteId) {
+		this.clienteId = clienteId;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getColonia() {
+		return colonia;
+	}
+
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEstatus() {
+		return estatus;
+	}
+
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	public Timestamp getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Timestamp fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public Timestamp getFechaBaja() {
+		return fechaBaja;
+	}
+
+	public void setFechaBaja(Timestamp fechaBaja) {
+		this.fechaBaja = fechaBaja;
+	}
+
+	public int getSucursalId() {
+		return sucursalId;
+	}
+
+	public void setSucursalId(int sucursalId) {
+		this.sucursalId = sucursalId;
+	}
+
+	public int getEmpleadoId() {
+		return empleadoId;
+	}
+
+	public void setEmpleadoId(int empleadoId) {
+		this.empleadoId = empleadoId;
+	}
 
 }
