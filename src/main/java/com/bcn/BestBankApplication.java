6d0fded3a414d9c6bbd7ac32fc577b78b245be16
victor <vramirez.info@gmail.com>
2023-12-01 18:38:36 -0600

    validations_pageserrorcustoms

--- src/main/java/com/bcn/BestBankApplication.java
+++ src/main/java/com/bcn/BestBankApplication.java
@@ -1,12 +1,27 @@
 package com.bcn;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class BestBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BestBankApplication.class, args);
+		SpringApplication app = new SpringApplication(BestBankApplication.class); 
+		Map<String,Object> map = new HashMap<>();
+        map.put("server.port", "8080");
+        //map.put("server.adress", "0.0.0.0");       
+        app.setDefaultProperties(map);
+        app.run(args);
 	}
 }
+
+
+
+        
+        
